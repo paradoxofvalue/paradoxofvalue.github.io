00385e4b38594985acaf15e3b59c975dd87dfae2
Vadim Lavorchuk <vadim.lavorchuk@richlodesolutions.com>
2017-06-13 17:23:41 +0300

    Updates

--- bundles/Page/components/Footer/FooterContainer.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by pow on 13.06.17.
- */
-import React from 'react';
-import Footer from "./Footer";
-
-export default () => <Footer />;
--- bundles/Page/components/Header/HeaderContainer.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by pow on 13.06.17.
- */
-import React from 'react';
-import Header from "./Header";
-
-export default () => <Header />;
